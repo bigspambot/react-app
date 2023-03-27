751d2907f28dcb88b8bcd042d0dc1f761b98c57c
bigspambot <chazbish@hotmail.com>
2023-03-27 17:03:57 +0100

    Up to Fragments

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,10 +1,10 @@
 // PascalCasing
 function Message() {
     // JSX: JavaScript XML
-    const name = 'Charlie'
+    const name = 'Alice'
     if (name)
         return <h1>Hello {name}</h1>;
-    return <h1>Hellow World</h1>
+    return <h1>Hello World</h1>
 }
 
 export default Message;
